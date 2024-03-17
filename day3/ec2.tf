8bc18ea36a3f447dadc3c9e0222b326a8fdf8106
pravalika <Paruuuuu>
2024-03-17 15:03:02 +0000

    new

--- day3/ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance"  "yname" {
-    ami = "ami-05b8575d080b71baf"
-     key_name = "key3"
-    instance_type ="t3.micro"
-
-    tags = {
-        Name = "prabhas"
-    }
-}
--- day3/main.tf
+++ day3/main.tf
@@ -44,6 +44,56 @@ resource "aws_route_table" "myname" {
 resource "aws_route_table_association" "myname" {
     route_table_id = aws_route_table.myname.id
     subnet_id = aws_subnet.myname.id
-    
+
   
 }
+#create security groups
+  resource "aws_security_group" "myname" {
+  name        = "allow_tls"
+  vpc_id      = aws_vpc.myname.id
+
+  tags = {
+    Name = "my-security-group"
+  }
+ ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+
+  }
+  #create ec2 instance
+  resource "aws_instance" "yname" {
+      ami = "ami-0d7a109bf30624c99"
+     key_name = "super"
+    instance_type ="t2.micro"
+    associate_public_ip_address = true
+    security_groups = [ aws_security_group.myname.id ]
+
+subnet_id = aws_subnet.myname.id
+
+   
+  }
+
+   
+  
+
